216fa8be3d938019836164ae606ccfafc10e2f1d
Sudeep Sharma <sudeep@eztrips.in>
2026-04-21 23:23:09 +0600

    Revert "feat: add EnquiryForm, Navbar, About page, and WhatsApp floating button"
    This reverts commit 4b0025c3122eddce3523ddb683ab899e83d7ea00.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import WhatsAppButton from "@/components/WhatsAppButton";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,7 +30,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <WhatsAppButton />
       </body>
     </html>
   );
